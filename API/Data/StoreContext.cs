4b983a32d6a6a8740701f53b53ac272c1125f621
Joseph <yosephassefa85@gmail.com>
2021-08-22 06:29:40 -0700

    Initial Commit

---
+++ API/Data/StoreContext.cs
@@ -0,0 +1,16 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class StoreContext : DbContext
+    {
+
+        public StoreContext(DbContextOptions<StoreContext> options ): base
+            (options)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
